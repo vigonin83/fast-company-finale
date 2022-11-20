fe8fae7c814cbc6e0e85c16ab460d30eb0e8490c
Vasiliy Igonin <vigonin83@gmail.com>
2022-11-20 23:19:58 +0300

    create & remove comments

---
+++ src/app/components/ui/comments.jsx
@@ -0,0 +1,61 @@
+import { orderBy } from "lodash";
+import React, { useEffect } from "react";
+import CommentsList, { AddCommentForm } from "../common/comments";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    createNewComment,
+    getComments,
+    getCommentsLoadingStatus,
+    loadCommentsList,
+    removeCurrentComment
+} from "../../store/comments";
+import { useParams } from "react-router-dom";
+
+const Comments = () => {
+    const { userId } = useParams();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadCommentsList(userId));
+        dispatch(createNewComment(userId));
+        dispatch(removeCurrentComment(userId));
+    }, [userId]);
+    const isLoading = useSelector(getCommentsLoadingStatus());
+    const createComment = useSelector(createNewComment());
+    const removeComment = useSelector(removeCurrentComment());
+    const comments = useSelector(getComments());
+
+    const handleSubmit = (data) => {
+        createComment(data);
+    };
+    const handleRemoveComment = (id) => {
+        removeComment(id);
+    };
+    const sortedComments = orderBy(comments, ["created_at"], ["desc"]);
+    return (
+        <>
+            <div className="card mb-2">
+                <div className="card-body ">
+                    <AddCommentForm onSubmit={handleSubmit} />
+                </div>
+            </div>
+            {sortedComments.length > 0 && (
+                <div className="card mb-3">
+                    <div className="card-body ">
+                        <h2>Comments</h2>
+                        <hr />
+                        {!isLoading ? (
+                            <CommentsList
+                                comments={sortedComments}
+                                onRemove={handleRemoveComment}
+                            />
+                        ) : (
+                            "Loading..."
+                        )}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Comments;
---
+++ src/app/store/comments.js
@@ -0,0 +1,76 @@
+import { createSlice } from "@reduxjs/toolkit";
+import commentService from "../services/comment.service";
+import { nanoid } from "nanoid";
+const commentsSlice = createSlice({
+    name: "comments",
+    initialState: {
+        entities: null,
+        isLoading: true,
+        error: null
+    },
+    reducers: {
+        commentsRequested: (state) => {
+            state.isLoading = true;
+        },
+        commentsReceived: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        commentsRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        commentCreated: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        commentRemoved: (state, action) => {
+            state.entities = action.payload.filter((c) => c._id !== action.payload.id);
+        }
+    }
+});
+
+const { reducer: commentsReducer, actions } = commentsSlice;
+const { commentsRequested, commentsReceived, commentsRequestFailed, commentCreated, commentRemoved } = actions;
+
+export const loadCommentsList = (userId) => async (dispatch) => {
+    dispatch(commentsRequested());
+    try {
+        const { content } = await commentService.getComments(userId);
+        dispatch(commentsReceived(content));
+    } catch (error) {
+        dispatch(commentsRequestFailed(error.message));
+    }
+};
+
+export const createNewComment = (data) => async (dispatch) => {
+    const comment = {
+        ...data,
+        _id: nanoid(),
+        pageId: data.userId,
+        created_at: Date.now(),
+        userId: data.currentUserId
+    };
+    try {
+        const { content } = await commentService.createComment(comment);
+        dispatch(commentCreated(content));
+    } catch (error) {
+        dispatch(commentsRequestFailed(error.message));
+    }
+    console.log(comment);
+};
+export const removeCurrentComment = (id) => async (dispatch) => {
+    try {
+        const { content } = await commentService.removeComment(id);
+        if (content === null) {
+            dispatch(commentRemoved(id));
+        }
+    } catch (error) {
+        dispatch(commentsRequestFailed(error.message));
+    }
+};
+export const getComments = () => (state) => state.comments.entities;
+export const getCommentsLoadingStatus = () => (state) =>
+    state.comments.isLoading;
+
+export default commentsReducer;
